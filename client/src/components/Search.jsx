570305b53e1fd982871c23906912b99f958b95cf
Bastien-Baloup <35816948+Bastien-Baloup@users.noreply.github.com>
2020-08-04 23:36:33 +0200

    ajout css de la searchbar et des bouttons filtrer et trier

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -3,6 +3,8 @@ import algoliasearch from 'algoliasearch/lite'
 import { InstantSearch, Configure, SearchBox, SortBy, CurrentRefinements } from 'react-instantsearch-dom'
 import Filters from './Filters'
 import MyInfiniteHits from './InfiniteHits'
+import { faSlidersH } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Search = ({ category }) => {
   const searchClient = algoliasearch('H2YGA5NBNG', '26b1badfb8cb68a219e300f7cd17df1e')
@@ -19,17 +21,18 @@ const Search = ({ category }) => {
         onSearchStateChange={onSearchStateChange}
         searchState={searchState}
       >
-        {category && <Configure filters={'categories:"' + category + '"'} />}
-        <SearchBox />
-        <SortBy
-          items={[
-            { value: 'demo_ecommerce', label: 'Price desc' },
-            { value: 'price_asc', label: 'Price asc' },
-            { value: 'popularity', label: 'Popularity' }
-          ]}
-          defaultRefinement='popularity'
-        />
-        <button onClick={() => setIsModalOpen(true)}>Filtrer</button>
+        {category ? <Configure filters={'categories:"' + category + '"'} /> : <SearchBox />}
+        <div className='buttons'>
+          <button onClick={() => setIsModalOpen(true)}>Filtrer <FontAwesomeIcon icon={faSlidersH} /></button>
+          <SortBy
+            items={[
+              { value: 'demo_ecommerce', label: 'Price desc' },
+              { value: 'price_asc', label: 'Price asc' },
+              { value: 'popularity', label: 'Popularity' }
+            ]}
+            defaultRefinement='popularity'
+          />
+        </div>
         <CurrentRefinements />
         <MyInfiniteHits />
 
