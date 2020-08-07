1954ba5e2c1c426edfa5b60416bae7a8f83b7d15
Bastien-Baloup <35816948+Bastien-Baloup@users.noreply.github.com>
2020-08-07 18:50:00 +0200

    css pages de recherche + petites corrections

--- client/src/components/Filters.jsx
+++ client/src/components/Filters.jsx
@@ -5,31 +5,43 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 ReactModal.setAppElement('#root')
-const VirtualRefinementList = connectRefinementList(() => null)
+const VirtualRefinementList0 = connectRefinementList(() => null)
+const VirtualRefinementList1 = connectRefinementList(() => null)
 const VirtualRange = connectRange(() => null)
 
 const Filters = ({ withCategories, searchClient, searchState, onSearchStateChange, isOpen, setIsOpen }) => {
   return (
     <>
-      <VirtualRefinementList attribute='brand' />
-      {withCategories && <VirtualRefinementList attribute='categories' />}
+      <VirtualRefinementList0 attribute='brand' />
+      {withCategories && <VirtualRefinementList1 attribute='categories' />}
       <VirtualRange attribute='price' />
-      <InstantSearch
-        searchClient={searchClient}
-        indexName='demo_ecommerce'
-        onSearchStateChange={onSearchStateChange}
-        searchState={searchState}
+      <ReactModal
+        isOpen={isOpen}
+        onRequestClose={() => setIsOpen(false)}
       >
-        <ReactModal
-          isOpen={isOpen}
-          onRequestClose={() => setIsOpen(false)}
+        <button className='close' onClick={() => setIsOpen(false)}><FontAwesomeIcon icon={faTimes} /></button>
+        <InstantSearch
+          searchClient={searchClient}
+          indexName='demo_ecommerce'
+          onSearchStateChange={onSearchStateChange}
+          searchState={searchState}
         >
-          <button onClick={() => setIsOpen(false)}><FontAwesomeIcon icon={faTimes} /></button>
-          <RefinementList attribute='brand' />
-          {withCategories && <RefinementList attribute='categories' />}
-          <RangeInput attribute='price' />
-        </ReactModal>
-      </InstantSearch>
+          <div className='brand'>
+            <h3>Marques :</h3>
+            <RefinementList attribute='brand' limit={200} />
+          </div>
+          {withCategories &&
+            <div className='category'>
+              <h3>Categories :</h3>
+              <RefinementList attribute='categories' limit={200} />
+            </div>
+          }
+          <div className='price'>
+            <h3>Prix :</h3>
+            <RangeInput attribute='price' translations={{separator: '-'}} />
+          </div>
+        </InstantSearch>
+      </ReactModal>
     </>
   )
 }
